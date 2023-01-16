d53dac8444abb0e9aa3a45bff306f0879e856a26
januaryandi <yandisephri@gmail.com>
2023-01-16 17:56:06 +0700

    first commit

---
+++ FDA_E020320063/app/src/main/java/id/ac/poliban/mi/VC/e020320063/fda_e020320063/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.VC.e020320063.fda_e020320063.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project77_E020320063/app/src/main/java/id/ac/poliban/mi/VC/e020320063/project_e020320063/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.VC.e020320063.project_e020320063;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
